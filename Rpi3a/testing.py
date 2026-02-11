34ecb57bad16e248315fe3a67ef2843513d70050
aneeshmat <aneeshmat@vt.edu>
2026-02-10 23:08:02 -0500

    testing update

--- Rpi3a/testing.py
+++ Rpi3a/testing.py
@@ -11,3 +11,10 @@ with sd.InputStream(device=MIC_ID, channels=1, samplerate=RATE, blocksize=BLOCK)
         data, _ = s.read(BLOCK)
         rms = float(np.sqrt(np.mean(data[:,0]**2)))
         print("RMS:", rms)
+
+import sounddevice as sd
+print(sd.query_devices(4))
+
+import sounddevice as sd
+for i, d in enumerate(sd.query_devices()):
+    print(i, d['name'], d['max_input_channels'], d['max_output_channels'])
